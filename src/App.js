40b9f6eb340114fa2527bf69cb8a55b2b398d1f3
Domenic O’Neill <domeniconeill@Domenics-MacBook-Pro-2.local>
2021-04-21 15:48:46 -0500

    yo

--- src/App.js
+++ src/App.js
@@ -1,25 +1,30 @@
+import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+class App extends React.Component {
 
+  constructor() {
+    super();
+    this.state = {
+      string: 'Hello Dom',
+      r: 0
+    }
+    setInterval(() => {
+      this.setState({r: this.state.r + 1})
+    }, 4600);
+  }
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <p>{ this.state.string }</p> {this.state.r}
+          <button onClick={() => { this.setState({string: 'Yo Dom'})}}>Change Text</button>
+        </header>
+      </div>
+    );
+  }
+}
+// module.exports.App = App
 export default App;
